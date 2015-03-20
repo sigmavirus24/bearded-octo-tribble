3169848c45ea07a5c13d8f6b6bcb4735c97bb5a3
Ian Cordasco <ian.cordasco@rackspace.com>
2015-03-19 21:46:33 -0500

    Use openvas where appropriate

--- openvas/openvas_reports/views.py
+++ openvas/openvas_reports/views.py
@@ -12,37 +12,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import json
-
 from django.core import urlresolvers
 from django.views.generic.base import View
 from django.http import HttpResponse, HttpResponseBadRequest
-from horizon.tables import DataTableView
+import horizon.tables
 
 from openvas.openvas_reports import tables
 from openvas import omp
 
 
-class IndexView(DataTableView):
+class IndexView(horizon.tables.DataTableView):
     table_class = tables.ReportsTable
-    template_name = 'rackspace/openvas/index.html'
-
-    def get(self, request, *args, **kwargs):
-        context = self.get_context_data(**kwargs)
-
-        tables = []
-        for (name, table) in list(context.items()):
-            if name.endswith('_table'):
-                del context[name]
-                table.data.parameters = json.dumps(
-                    table.data.get_parameter_types(self.request))
-                table.data.launch_url = urlresolvers.reverse(
-                    'horizon:rackspace:heat_store:launch',
-                    args=[table.data.id])
-                tables.append(table)
-
-        context['tables'] = tables
-        return self.render_to_response(context)
+    template_name = 'openvas/openvas/index.html'
 
     def get_tables(self):
         return dict(('Reports', self.table_class(self.request, t))
